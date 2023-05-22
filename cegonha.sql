aa3eea9b42e250496fd803a66494175ef0acf33a
Leonardo Monteiro <leo.monteiro06@live.com>
2023-05-22 16:11:34 -0300

    Update cegonha.sql

--- cegonha.sql
+++ cegonha.sql
@@ -1,17 +1,17 @@
- CREATE TABLE cegonha(
+CREATE TABLE cegonha(
     id_user INT AUTO_INCREMENT NOT NULL,
     name_user VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     cpf VARCHAR(11) NOT NULL,
     PRIMARY KEY(id_user)
-  );
+);
 
-  CREATE TABLE buy(
+CREATE TABLE buy(
     buyer INT NOT NULL,
     id_order INT AUTO_INCREMENT NOT NULL,
     name_buyer VARCHAR(50) NOT NULL,
-    price DECIMAL(5,2) NOT NULL,
+    price INT NOT NULL,
     street VARCHAR(50) NOT NULL,
     num INT NOT NULL,
     district VARCHAR(50) NOT NULL,
@@ -21,9 +21,9 @@
     CONSTRAINT fk_buyer 
     FOREIGN KEY (buyer) REFERENCES cegonha(id_user)
     ON DELETE CASCADE
-  );
-  
-  SELECT c.id_user,c.name_user,c.email,b.name_buyer,b.category,b.price,b.street,b.number,b.district,b.city,b.state
-  from cegonha AS c 
-  RIGHT JOIN buy AS b 
-  ON c.id_user = b.id_order;
+);
+    
+SELECT c.id_user,c.name_user,c.email,b.name_buyer,b.category,b.price,b.street,b.number,b.district,b.city,b.state
+from cegonha AS c 
+RIGHT JOIN buy AS b 
+ON c.id_user = b.id_order;
