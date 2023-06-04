8efea871aebb1b36a42fde688dce90b855205001
Leonardo Monteiro <leo.monteiro06@live.com>
2023-06-04 08:56:06 -0300

    Update

--- web/php/login.php
+++ web/php/login.php
@@ -1,53 +1,53 @@
 <?php
-$email = $_POST["email"];
-$password = $_POST["password"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-$servername = "localhost";
-$username = "root";
-$password_db = "";
-$dbname = "projeto";
+    $servername = "localhost";
+    $username = "root";
+    $password_db = "";
+    $dbname = "projeto";
 
-$conn = new mysqli($servername, $username, $password_db, $dbname);
+    $conn = new mysqli($servername, $username, $password_db, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-$sql = "SELECT name_user, email, password FROM cegonha WHERE email=?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
+    $sql = "SELECT name_user, email, password FROM cegonha WHERE email=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $hashedPassword = $row["password"];
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row["password"];
 
-    if (password_verify($password, $hashedPassword)) {
-        echo "<script>
-                while (profileName.length) { profileName.pop(); }
-                profileName.push('" . $row["name_user"] . "')
-                localStorage.setItem('profileName', JSON.stringify(profileName));
-                alertify.success('Logado com sucesso!');
-                location.hash = '';
-                document.getElementById('login').remove();
-                document.getElementById('profile').style.display = 'block';
-                userEmail.push('$email');
-                localStorage.setItem('userEmail', JSON.stringify(userEmail));
-            </script>";
+        if (password_verify($password, $hashedPassword)) {
+            echo "<script>
+                    while (profileName.length) { profileName.pop(); }
+                    profileName.push('" . $row["name_user"] . "')
+                    localStorage.setItem('profileName', JSON.stringify(profileName));
+                    alertify.success('Logado com sucesso!');
+                    location.hash = '';
+                    document.getElementById('login').remove();
+                    document.getElementById('profile').style.display = 'block';
+                    userEmail.push('$email');
+                    localStorage.setItem('userEmail', JSON.stringify(userEmail));
+                </script>";
+        } else {
+            echo "<script>
+                    alertify.error('Senha incorreta');
+                    location.hash = '';
+                </script>";
+        }
     } else {
         echo "<script>
-                alertify.error('Senha incorreta');
+                alertify.error('Este usuário não existe');
                 location.hash = '';
             </script>";
     }
-} else {
-    echo "<script>
-            alertify.error('Este usuário não existe');
-            location.hash = '';
-        </script>";
-}
 
-$stmt->close();
-$conn->close();
+    $stmt->close();
+    $conn->close();
 ?>
